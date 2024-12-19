3852cda6fd54c314dae324940cc7db278a9677eb
Edamadakvishnu <edamadakavishnu@gmail.com>
2024-12-19 18:49:16 +0530

    My Third program

---
+++ fun.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int add(int a, int b) 
+{
+    return a + b;
+}
+
+int main() 
+{
+    int num1, num2, result;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    result = add(num1, num2);
+    printf("Sum of %d and %d is: %d\n", num1, num2, result);
+    return 0;
+}
